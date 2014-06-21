a67ad6d339eea65efaac4d0dea0cb392d1b6a6fa
ukiuni <ukiuni@gmail.com>
2014-06-21 17:29:40 +0900

    v30 builded.

--- bin/pacifista.bat
+++ bin/pacifista.bat
@@ -1,2 +1,2 @@
 set basePath=%~dp0
-java -cp %basePath%pacifista.0.0.28.jar;%basePath%template;%basePath%..\libs\*;%basePath%..\plugins\*;%basePath%..\libs\aws\* org.ukiuni.pacifista.Main -baseDir %basePath%../ %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp %basePath%pacifista.0.0.30.jar;%basePath%template;%basePath%..\libs\*;%basePath%..\plugins\*;%basePath%..\libs\aws\* org.ukiuni.pacifista.Main -baseDir %basePath%../ %1 %2 %3 %4 %5 %6 %7 %8 %9
